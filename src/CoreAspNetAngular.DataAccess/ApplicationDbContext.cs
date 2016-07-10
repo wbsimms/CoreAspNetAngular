5f2852c23242cd495b8bbb7f53b5773d11cd0ba5
Wm. Barrett Simms <barrett@wbsimms.com>
2016-07-10 15:54:28 -0500

    Update

---
+++ src/CoreAspNetAngular.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreAspNetAngular.DataAccess.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreAspNetAngular.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+	    public DbSet<Case> Cases { get; set; }
+	    public DbSet<Organization> Organizations { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/CoreAspNetAngular.DataAccess/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CoreAspNetAngular.DataAccess.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+	    public string FirstName { get; set; }
+	    public string LastName { get; set; }
+	    public Organization Organization { get; set; }
+		public int OrganizationId { get; set; }
+	}
+}
